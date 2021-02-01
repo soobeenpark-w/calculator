1601157c1139fcb8e3b17ef11f4d00fa60b50f6d
eundonglee <cr3306@naver.com>
2021-02-01 17:58:19 +0900

    Revert "Merge branch 'feature/create-multi' into develop"
    This reverts commit faabfb5e98f240aa43559b6534de718a9b7ef74c, reversing
    changes made to ab8c2de2ca15610a7cd16c1577b8e70540d10182.

--- main.c
+++ main.c
@@ -47,7 +47,8 @@ int sub(int number1, int number2)
  
 int multi(int number1, int number2)
 {
-	return number1 * number2;
+	//TODO implementation
+	return 0;
 }
  
 int div(int number1, int number2)
